3821ab8b0b5eec800343ebbf81d1be4ba80f64d5
dev-mahbub <mahbub.devs@gmail.com>
2025-05-05 13:37:29 +0600

    updated

---
+++ function/scope.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int x = 5; // global variable
+int main()
+{
+    printf("%d", x);
+    return 0;
+}
---
+++ function/useful_math_function.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int ans = abs(-10);
+    // int ans = pow(4, 3);
+    // int ans = round(4.3);
+    // int ans = sqrt(49);
+    // int ans = floor(5.2);
+    // int ans = ceil(5.2);
+    printf("%d", ans);
+    return 0;
+}
