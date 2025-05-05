5da77326a6a57ddd43e4adf02e0eeb71c9fd2331
dev-mahbub <mahbub.devs@gmail.com>
2025-05-05 23:49:01 +0600

    4 problem solve

--- pattern_and_sorting/T_Shape_2.c
+++ pattern_and_sorting/T_Shape_2.c
@@ -19,20 +19,5 @@ int main()
         star += 2;
         space--;
     }
-    int rev_space = 0;
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= rev_space; j++)
-        {
-            printf(" ");
-        }
-        for (int j = star - 2; j >= 1; j--)
-        {
-            printf("*");
-        }
-        printf("\n");
-        star -= 2;
-        rev_space++;
-    }
     return 0;
 }
